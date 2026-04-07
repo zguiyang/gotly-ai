99fcfbdaa77b01c6ba32bcb92650d2e5a7c2cbc9
zguiyang <zhaoguiyang18@outlook.com>
2026-04-08 00:20:50 +0800

    feat: add initial Next.js app with layout and home page

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Gotly AI",
+  description: "Gotly AI is a lightweight AI-powered capture tool that turns one sentence into notes, todos, and bookmarks. Just say it, and it’s saved.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className={'text-foreground text-2xl'}>Welcome to Next.js!</h1>
+    </div>
+  );
+}
